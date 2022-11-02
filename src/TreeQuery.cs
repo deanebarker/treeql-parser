16739294b680a7c04fe2c29079965148674c58f8
Deane Barker <deane@deanebarker.net>
2022-11-02 10:22:45 +0000

    Moved the parser code in

---
+++ src/TreeQuery.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+
+namespace DeaneBarker.TreeQL
+{
+    public class TreeQuery
+    {
+        public List<Target> Targets { get; set; } = new List<Target>();
+        public List<Sort> Sort { get; set; } = new List<Sort>();
+        public long Limit { get; set; }
+        public int Skip { get; set; }
+        public string Tag { get; set; }
+        public List<Filter> Filters { get; set; } = new List<Filter>();
+
+        // Just for debugguing
+        public string Source { get; set; }
+    }
+
+    public class Filter
+    {
+        public string FieldName { get; set; }
+        public string Type { get; set; }
+        public string Operator { get; set; }
+        public string Value { get; set; }
+        public string Conjunction { get; set; }
+    }
+
+    public class Target
+    {
+        public string Scope { get; set; }
+        public string Path { get; set; }
+        public bool Inclusive { get; set; } // Whether or not the include the target
+    }
+
+    public class Sort
+    {
+        public string Value { get; set; }
+        public SortDirection Direction { get; set; }
+    }
+
+    public enum SortDirection
+    {
+        Ascending,
+        Descending
+    }
+}
---
+++ src/TreeQueryParser.cs
@@ -0,0 +1,183 @@
+﻿using Parlot;
+using Parlot.Fluent;
+using static Parlot.Fluent.Parsers;
+
+namespace DeaneBarker.TreeQL
+{
+    public static class TreeQueryParser
+    {
+
+        public static Func<TextSpan, bool> TargetValidator = (t) => { return true; }; // Validate anything by default
+        public static string TargetValidatorError = "Target must (1) begin and end with a forward slash, (2) be an integer, or (3) start with \"@\"";
+
+        public static string[] AllowedOperators = new[] { "=", "!=", ">", ">=", "<", "<=" };
+
+        private static string commentPrefix = "#";
+
+        private static Parser<TreeQuery> parser;
+
+        public static TreeQuery Parse(string q, object data)
+        {
+            foreach (var property in data.GetType().GetProperties())
+            {
+                var value = property.GetValue(data, null);
+                q = q.Replace(string.Concat("@", property.Name), value.ToString());
+            }
+
+            return Parse(q);
+        }
+
+        public static TreeQuery Parse(string q)
+        {
+            if(string.IsNullOrWhiteSpace(q))
+            {
+                return new TreeQuery();
+            }
+
+            q = Clean(q);
+            var query = parser.Parse(q);
+            query.Source = q;
+            return query;
+        }
+
+        private static string Clean(string input)
+        {
+            var lines = input.Split(new string[] { "\n", "\r\n", Environment.NewLine }, StringSplitOptions.None).AsQueryable();
+
+            lines = lines
+                .Where(l => !l.Trim().StartsWith(commentPrefix))
+                .Select(s => s.Trim());
+
+            return string.Join(" ", lines).ToLower().Trim(); 
+        }
+
+        static TreeQueryParser()
+        {
+            var of = Terms.Text("of");
+            var and = Terms.Text("and");
+            var or = Terms.Text("or");
+            var select = Terms.Text("select");
+
+
+            // Target
+            var children = Terms.Text("children");
+            var results = Terms.Text("results");
+            var parent = Terms.Text("parent");
+            var descendants = Terms.Text("descendants");
+            var ancestors = Terms.Text("ancestors");
+            var self = Terms.Text("self");
+            var siblings = Terms.Text("siblings");
+            var inclusive = Terms.Text("inclusive");
+            var exclusive = Terms.Text("exclusive");
+
+            var scope = ZeroOrOne(OneOf(results, parent, children, descendants, self, ancestors, siblings).ElseError("Expected scope")).Then(v =>
+            {
+                return v ?? "self";
+            });
+            var path = Terms.NonWhiteSpace().When(t => TargetValidator(t));
+            var target = 
+                scope // Item1
+                .AndSkip(of)
+                .And(path) // Item2
+                .And(ZeroOrOne(OneOf(inclusive, exclusive))) // Item3
+                .Then(v =>
+            {
+                return new Target()
+                {
+                    Scope = v.Item1,
+                    Path = v.Item2.ToString(),
+                    Inclusive = v.Item3 == "inclusive"
+                };
+            });
+
+
+
+            // Where clause
+            var conjunction = OneOf(and, or);
+            var where = Terms.Text("where");
+            var fieldName = Terms.NonWhiteSpace();
+            var contains = Terms.Text("contains");
+            var lessThan = Terms.Text("<");
+            var equals = Terms.Text("=");
+            var greaterThan = Terms.Text(">");
+            var notEqualTo = Terms.Text("!=");
+            var value = Terms.String(StringLiteralQuotes.SingleOrDouble);
+            var whereClause = ZeroOrOne(conjunction) // Item1
+                .And(fieldName) // Item2
+                //.And(Terms.NonWhiteSpace().When(t => AllowedOperators.Contains(t.ToString().Trim())).ElseError($"Allowed operators: {string.Join(", ", AllowedOperators)}")) // Item3
+                .And(OneOf(contains, lessThan, equals, greaterThan, notEqualTo))
+                .And(value) // Item4
+                .Then(v =>
+                {
+                    var fieldName = v.Item2.ToString().Split(':').First();
+                    var type = v.Item2.ToString().Contains(":") ? v.Item2.ToString().Split(':').Last() : "string";
+
+                    return new Filter()
+                    {
+                        Conjunction = v.Item1,
+                        FieldName = fieldName,
+                        Type = type,
+                        Operator = v.Item3.ToString(),
+                        Value = v.Item4.ToString()
+                    };
+                });
+
+
+
+            // Sort Separator
+            var order = Terms.Text("order");
+            var by = Terms.Text("by");
+            var sortSeparator = order.And(by);
+
+
+
+            // Sort Value
+            var ascending = Terms.Text("asc");
+            var descending = Terms.Text("desc");
+            var sortDirection = OneOf(ascending, descending);
+            var sortValue = SkipWhiteSpace(Literals.Pattern(c => char.IsLetterOrDigit(c) || c == ':')).And(ZeroOrOne(OneOf(Terms.Text("asc"), Terms.Text("desc")))).Then(v =>
+            {
+                return new Sort()
+                {
+                    Value = v.Item1.ToString(),
+                    Direction = v.Item2?.ToString() == "desc" ? SortDirection.Descending : SortDirection.Ascending
+                };
+            });
+
+
+
+            // Limit value
+            var limit = Terms.Text("limit");
+            var number = Terms.Integer();
+
+
+
+            // Full Command
+            parser =
+                select.ElseError("Expected \"select\"")
+                .And(Separated(and, target)).ElseError(TargetValidatorError) // Item 1
+                .AndSkip(ZeroOrOne(where))
+                .And(ZeroOrMany(whereClause)) // Item 2
+                .AndSkip(ZeroOrOne(sortSeparator))
+                .And(ZeroOrOne(Separated(Literals.Char(','), sortValue))) // Item 3
+                .And(ZeroOrOne(limit.SkipAnd(number))) // Item 4
+                .Then(v =>
+                {
+                    var query = new TreeQuery()
+                    {
+                        Targets = v.Item2,
+                        Sort = v.Item4 ?? new List<Sort>(),
+                        Limit = Convert.ToInt32(v.Item5.ToString())
+                    };
+
+                    foreach (var item in v.Item3)
+                    {
+                        query.Filters.Add(item);
+                    }
+
+                    return query;
+
+                });
+        }
+    }
+}
