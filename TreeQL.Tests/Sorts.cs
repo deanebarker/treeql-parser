f4ebf657573fd594c7de63b3659eae376ab6d864
Deane <deane@deanebarker.net>
2022-11-03 11:13:20 +0000

    More tests

--- TreeQL.Tests/Sorts.cs
+++ TreeQL.Tests/Sorts.cs
@@ -15,5 +15,31 @@ namespace TreeQL.Tests
             Assert.AreEqual("name", q.Sort.First().Value);
             Assert.AreEqual(SortDirection.Ascending, q.Sort.First().Direction);
         }
+
+        [TestMethod]
+        public void MultipleSorts()
+        {
+            var q = TreeQueryParser.Parse("SELECT children OF target ORDER BY name, age");
+
+            Assert.AreEqual(2, q.Sort.Count());
+            Assert.AreEqual("age", q.Sort[1].Value);
+        }
+
+        [TestMethod]
+        public void DescendingSort()
+        {
+            var q = TreeQueryParser.Parse("SELECT children OF target ORDER BY name DESC");
+
+            Assert.AreEqual(SortDirection.Descending, q.Sort.First().Direction);
+        }
+
+        [TestMethod]
+        public void DescendingSortWithMultipleSorts()
+        {
+            var q = TreeQueryParser.Parse("SELECT children OF target ORDER BY name DESC, age");
+
+            Assert.AreEqual(SortDirection.Descending, q.Sort.First().Direction);
+            Assert.AreEqual(SortDirection.Ascending, q.Sort[1].Direction);
+        }
     }
 }
